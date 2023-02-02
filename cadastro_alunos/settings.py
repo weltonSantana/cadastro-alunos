57f4ecd794e1bb370b3d6b63bdea4ebc8d7bc285
weltonSantana <welton.santana@mail.uft.edu.br>
2023-02-02 09:43:14 -0300

    Configuração das variaveis de ambiente

--- cadastro_alunos/settings.py
+++ cadastro_alunos/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@d5-d&s@7zk^k8kpu3zl1rhg$4%(o6ybq0g3n+kss^yi84__r='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
